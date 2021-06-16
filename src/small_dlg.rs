c9febb6fcee8fa39db117943fedeecd5ded4caaa
Roman Uvarov <uvarovrs40@gmail.com>
2021-06-16 16:59:46 +0300

    small dialogs are positioned in the parent window center

--- src/small_dlg.rs
+++ src/small_dlg.rs
@@ -1,14 +1,30 @@
 use fltk::{prelude::WidgetExt, window::Window};
 
-pub fn show_err_msg(source: &Window, msg: &str) {
-    fltk::dialog::alert(source.w() / 2, source.h() / 2, &msg);
+pub fn show_err_msg(parent_window: &Window, msg: &str) {
+    let (x, y) = count_box_pos(parent_window, msg);
+
+    fltk::dialog::alert(x, y, &msg);
 }
 
-pub fn confirm_with_dlg(source: &Window, question: &str) -> bool {
-    let ans = fltk::dialog::choice(source.w() / 2, source.h() / 2, &question, "Да", "Нет","");
+pub fn confirm_with_dlg(parent_window: &Window, question: &str) -> bool {
+    let (x, y) = count_box_pos(parent_window, question);
+
+    let ans = fltk::dialog::choice(x, y, &question, "Да", "Нет","");
+
     ans == 0
 }
 
-pub fn show_info_msg(source: &Window, msg: &str) {
-    fltk::dialog::message(source.w() / 2, source.h() / 2, &msg);
+pub fn show_info_msg(parent_window: &Window, msg: &str) {
+    let (x, y) = count_box_pos(parent_window, msg);
+
+    fltk::dialog::message(x, y, &msg);
+}
+
+fn count_box_pos(parent_window: &Window, text: &str) -> (i32, i32) {
+    let (text_w, text_h) = fltk::draw::measure(text, true);
+
+    let x = parent_window.x() + parent_window.w() / 2 - text_w / 2;
+    let y = parent_window.y() + parent_window.h() / 2 - text_h / 2;
+
+    (x, y)
 }
