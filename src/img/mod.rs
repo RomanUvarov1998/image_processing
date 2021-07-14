a4ced39f76b576c6b00878a674e6b76855f4e7ae
Roman Uvarov <uvarovrs40@gmail.com>
2021-07-15 01:43:20 +0300

    added rows iterator

--- src/img/mod.rs
+++ src/img/mod.rs
@@ -427,6 +427,97 @@ impl Iterator for PixelsIterator {
 }
 
 
+pub struct RowsIter {
+    left_col: usize,
+    right_col_excluded: usize,
+    max_row_excluded: usize,
+    cur_row: usize,
+}
+
+impl RowsIter {
+    pub fn new(area: PixelsArea) -> Self {
+        RowsIter {
+            left_col: area.top_left().col,
+            right_col_excluded: area.bottom_right().col,
+            max_row_excluded: area.bottom_right().row,
+            cur_row: area.top_left().col,
+        }
+    }
+}
+
+impl Iterator for RowsIter {
+    type Item = PixelsRow;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let curr_row = self.cur_row;
+
+        self.cur_row += 1;
+
+        if curr_row < self.max_row_excluded {
+            let left = PixelPos::new(curr_row, self.left_col);
+            let right = PixelPos::new(curr_row, self.right_col_excluded);
+            Some(PixelsRow::new(left, right))
+        } else {
+            None
+        }
+    }
+}
+
+
+pub struct PixelsRow {
+    left: PixelPos,
+    right: PixelPos
+}
+
+impl PixelsRow {
+    pub fn new(left: PixelPos, right: PixelPos) -> Self {
+        assert!(left.row == right.row);
+        assert!(left.col <= right.col);
+        PixelsRow {
+            left,
+            right
+        }
+    }
+
+    pub fn get_cols_iter(&self) -> ColsIter {
+        ColsIter::new(self)
+    }
+}
+
+
+pub struct ColsIter {
+    row: usize,
+    right_col_excluded: usize,
+    cur_col: usize,
+}
+
+impl ColsIter {
+    pub fn new(row: &PixelsRow) -> Self {
+        ColsIter {
+            row: row.left.row,
+            right_col_excluded: row.right.col,
+            cur_col: row.left.col,
+        }
+    }
+}
+
+impl Iterator for ColsIter {
+    type Item = PixelPos;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let cur_col = self.cur_col;
+
+        self.cur_col += 1;
+
+        if cur_col < self.right_col_excluded {
+            Some(PixelPos::new(self.row, cur_col))
+        } else {
+            None
+        }
+    }
+}
+
+
 pub struct LayersIterator<'own> {
     img: &'own Img,
     curr_layer_num: usize
@@ -468,5 +559,13 @@ pub struct PixelsArea {
 impl PixelsArea {
     pub fn new(top_left: PixelPos, bottom_right: PixelPos) -> Self {
         PixelsArea { top_left, bottom_right }
+    }
+
+    pub fn top_left(&self) -> PixelPos {
+        self.top_left
+    }
+
+    pub fn bottom_right(&self) -> PixelPos {
+        self.bottom_right
     }
 }
